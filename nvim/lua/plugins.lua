361e481654070556220ef3de0aa85456f52a88f7
Willow Hayward <WillHayCode@gmail.com>
2023-03-11 19:10:21 +1000

    Changed dashboard plugin

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -28,25 +28,38 @@ require("lazy").setup({
     'Yggdroot/indentLine', -- Vertical lines to visually indicate indentation levels
     'sainnhe/edge', -- Color scheme
     {
-        'glepnir/dashboard-nvim', -- Dashboard on launch
+        'goolord/alpha-nvim', -- Dashboard on launch
         event = 'VimEnter',
-        config = function()
-        require('dashboard').setup {
-            theme = 'hyper',
-            config = {
-                header = {
-		    ':::       ::: ::::::::::: :::        :::        ::::::::  :::       ::: ::: ',
-		    ':+:       :+:     :+:     :+:        :+:       :+:    :+: :+:       :+: :+: ',
-		    '+:+       +:+     +:+     +:+        +:+       +:+    +:+ +:+       +:+ +:+ ',
-		    '+#+  +:+  +#+     +#+     +#+        +#+       +#+    +:+ +#+  +:+  +#+ +#+ ',
-		    '+#+ +#+#+ +#+     +#+     +#+        +#+       +#+    +#+ +#+ +#+#+ +#+ +#+ ',
-		    ' #+#+# #+#+#      #+#     #+#        #+#       #+#    #+#  #+#+# #+#+#      ',
-		    '  ###   ###   ########### ########## ########## ########    ###   ###   ### ',
-		}
+        opts = function()
+            local dashboard = require("alpha.themes.dashboard")
+            local logo = [[
+                :::       ::: ::::::::::: :::        :::        ::::::::  :::       ::: ::: 
+                :+:       :+:     :+:     :+:        :+:       :+:    :+: :+:       :+: :+: 
+                +:+       +:+     +:+     +:+        +:+       +:+    +:+ +:+       +:+ +:+ 
+                +#+  +:+  +#+     +#+     +#+        +#+       +#+    +:+ +#+  +:+  +#+ +#+ 
+                +#+ +#+#+ +#+     +#+     +#+        +#+       +#+    +#+ +#+ +#+#+ +#+ +#+ 
+                 #+#+# #+#+#      #+#     #+#        #+#       #+#    #+#  #+#+# #+#+#      
+                  ###   ###   ########### ########## ########## ########    ###   ###   ### 
+            ]]
+            dashboard.section.header.val = vim.split(logo, "\n")
+            dashboard.section.buttons.val = {
+                dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
+                dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
+                dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+                dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
+                dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
+                dashboard.button("q", " " .. " Quit", ":qa<CR>"),
             }
-        }
-        end,
-        dependencies = { {'nvim-tree/nvim-web-devicons'}}
+            dashboard.section.footer.opts.hl = "Type"
+            dashboard.section.header.opts.hl = "AlphaHeader"
+            dashboard.section.buttons.opts.hl = "AlphaButtons"
+            dashboard.opts.layout[1].val = 8
+            dashboard.section.footer.val = 'Speaking from experience, Willow is very tall' 
+            return dashboard
+	    end,
+        config = function(_, dashboard)
+            require("alpha").setup(dashboard.opts)
+        end
     }
 })
 
