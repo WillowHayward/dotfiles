300309b0608b20383a822af38527d4b118a997c8
Willow Hayward <WillHayCode@gmail.com>
2023-03-12 17:16:17 +1000

    Fixed lsp/mason config

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -4,7 +4,7 @@ local language_servers = {
     "shfmt",
     "flake8",
     "typescript-language-server",
-    "tsserver"
+    "svelte-language-server"
 }
 
 -- TODO Leftover imports from last config, investigate
@@ -51,7 +51,56 @@ return {
             "mason.nvim",
             "williamboman/mason-lspconfig.nvim",
             "hrsh7th/cmp-nvim-lsp",
+            "jose-elias-alvarez/typescript.nvim"
         },
+        opts = {
+            servers = {},
+            setup = {
+                tsserver = function(_, opts)
+                    require("typescript").setup({ server = opts })
+                    return true
+                end,
+            }
+        },
+        config = function(_, opts)
+            -- This is mostly lifted from LazyVim
+	        local mlsp = require("mason-lspconfig")
+            local available = mlsp.get_available_servers()
+
+            local ensure_installed = {} ---@type string[]
+            local servers = opts.servers
+            local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local function setup(server)
+                local server_opts = vim.tbl_deep_extend("force", {
+                    capabilities = vim.deepcopy(capabilities),
+                }, servers[server] or {})
+
+                if opts.setup[server] then
+                    if opts.setup[server](server, server_opts) then
+                        return
+                    end
+                elseif opts.setup["*"] then
+                    if opts.setup["*"](server, server_opts) then
+                        return
+                    end
+                end
+                require("lspconfig")[server].setup(server_opts)
+            end
+            for server, server_opts in pairs(servers) do
+                if server_opts then
+                    server_opts = server_opts == true and {} or server_opts
+                      -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+                    if server_opts.mason == false or not vim.tbl_contains(available, server) then
+                        setup(server)
+                    else
+                        ensure_installed[#ensure_installed + 1] = server
+                    end
+                end
+            end
+
+	    require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+	    require("mason-lspconfig").setup_handlers({ setup })
+      end
     },
     
   -- formatters
@@ -71,17 +120,14 @@ return {
         end,
     },
     {
-        "williamboman/mason-lspconfig",
-        dependencies = {
-            "williamboman/mason.nvim"
-        },
+        "williamboman/mason.nvim",
         cmd = "Mason",
         opts = {
             ensure_installed = language_servers
         },
         ---@param opts MasonSettings | {ensure_installed: string[]}
         config = function(plugin, opts)
-            require("mason").setup(opts)
+            require("mason").setup()
             local mr = require("mason-registry")
             for _, tool in ipairs(opts.ensure_installed) do
                 local p = mr.get_package(tool)
@@ -91,11 +137,5 @@ return {
             end
         end,
     },
-    {
-        "jose-elias-alvarez/typescript.nvim",
-        config = function()
-            require("typescript").setup({})
-        end
-    }
 }
 
