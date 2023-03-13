0e8782c77cb4b80ab2d7382bd658949aa76bffad
Willow Hayward <willow@whc.fyi>
2023-03-13 21:14:43 +1000

    Fixed setup.sh, expanded gitconfig

--- setup.sh
+++ setup.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+# Assume $HOME/ as base for destinations
 declare -A links
-links[.zshrc]="~/.zshrc"
-links[.gitconfig]="~/.gitconfig"
-links[.gitignore.global]="~/.gitignore"
-links[.tmux.conf]="~/.tmux.conf"
-links[nvim]="~/.config/nvim"
-links[.npmrc]="~/.npmrc"
-links[.vimrc]="~/.vimrc"
+links[.zshrc]=".zshrc"
+links[.gitconfig]=".gitconfig"
+links[.tmux.conf]=".tmux.conf"
+links[nvim]=".config/nvim"
+links[.npmrc]=".npmrc"
+links[.vimrc]=".vimrc"
 
 for link in "${!links[@]}"; do
-    echo Linking $link
-    ln -s ${links[$link]} $link 
+    TARGET=$PWD/$link 
+    DESTINATION=$HOME/${links[$link]} 
+    if [[ -f "$DESTINATION" || -d "$DESTINATION" ]]; then
+        echo $DESTINATION already exists, skipping
+    else
+        echo Linking $TARGET to $DESTINATION 
+        ln -s $TARGET $DESTINATION
+    fi
 done
