6f45f19bb6a73c044c4489ce05ed780eb3e19e30
Willow Hayward <willow@whc.fyi>
2024-05-28 13:27:20 +1000

    Improved copilot integration into neovim

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,17 +1,34 @@
--- Function to set keymaps
+-- Function to set keymaps w/ some logical defaults
 local function set_keymap(mode, key, action, desc, opts)
-    local defaults = {
-        desc = desc or "",
-        silent = false,
-        expr = false,
-        noremap = true,
-    }
-    local fullOpts = vim.tbl_extend("force", defaults, opts or {})
-    vim.keymap.set(mode, key, action, fullOpts)
+	local defaults = {
+		desc = desc or "",
+		silent = false,
+		expr = false,
+		noremap = true,
+	}
+	local fullOpts = vim.tbl_extend("force", defaults, opts or {})
+	vim.keymap.set(mode, key, action, fullOpts)
 end
+
+-- Clear notifications
+set_keymap("n", "<leader><leader>", "<cmd>Noice dismiss<CR>", "Clear noice notifications") -- TODO: Does double-space scan?
+
+-- Refresh
+set_keymap("n", "<leader>e", "<cmd>e<CR>", "Reload buffer");
+
 -- Indenting a visual selection remains in visual mode afterwards
 set_keymap("v", ">", ">gv")
 set_keymap("v", "<", "<gv")
+
+-- Import the 'comment' module
+-- local comment = require("Comment")
+-- comment.setup
+
+set_keymap("n", "<leader>L", ":lua ", "Open Lua prompt")
+
+-- Set keymaps for commenting and uncommenting code
+-- set_keymap("n", "<leader>/", comment.toggle, "Toggle comment")
+-- set_keymap("v", "<leader>/", comment.toggle, "Toggle comment")
 
 -- provide hjkl movements in Insert mode via the <Alt> modifier key
 set_keymap("i", "<A-h>", "<C-o>h")
@@ -53,28 +70,28 @@ set_keymap("x", "s", substitute.visual, "Subsitute selection with contents of re
 
 -- substitute + yank BUG: Not currently working
 set_keymap(
-    "n",
-    "<leader>s",
-    sub(true, "operator"),
-    "[Broken]Subsitute text with contents of register and yank deleted text"
+	"n",
+	"<leader>s",
+	sub(true, "operator"),
+	"[Broken]Subsitute text with contents of register and yank deleted text"
 )
 set_keymap(
-    "n",
-    "<leader>ss",
-    sub(true, "line"),
-    "[Broken]Subsitute line with contents of register and yank deleted text"
+	"n",
+	"<leader>ss",
+	sub(true, "line"),
+	"[Broken]Subsitute line with contents of register and yank deleted text"
 )
 set_keymap(
-    "n",
-    "<leader>S",
-    sub(true, "eol"),
-    "[Broken]Subsitute text until end of line with contents of register and yank deleted text"
+	"n",
+	"<leader>S",
+	sub(true, "eol"),
+	"[Broken]Subsitute text until end of line with contents of register and yank deleted text"
 )
 set_keymap(
-    "x",
-    "<leader>s",
-    sub(true, "visual"),
-    "[Broken]Subsitute selection with contents of register and yank deleted text"
+	"x",
+	"<leader>s",
+	sub(true, "visual"),
+	"[Broken]Subsitute selection with contents of register and yank deleted text"
 )
 
 -- cellular automation (silly)
@@ -143,6 +160,7 @@ set_keymap("n", "[h", diagnostic_goto(false, "HINT"), "Previous hint")
 set_keymap("n", "]i", diagnostic_goto(true, "INFO"), "Next info")
 set_keymap("n", "[i", diagnostic_goto(false, "INFO"), "Previous info")
 set_keymap("n", "<leader>R", vim.lsp.buf.rename, "Rename current symbol")
+set_keymap("n", "<leader>r", vim.lsp.buf.rename, "Rename current symbol") -- TODO: Don't need both of these. Make a call, bucko
 
 set_keymap("n", "<leader>cs", vim.lsp.buf.document_symbol, "View document symbols")
 -- TypeScript
@@ -160,7 +178,6 @@ set_keymap("n", "<leader>ds", dap.step_over, "Step through code")
 set_keymap("n", "<leader>dS", dap.step_back, "Step back through code")
 set_keymap("n", "<leader>di", dap.repl.open, "Inspect debug state")
 
-
 local jester = require("jester")
 -- Jest
 set_keymap("n", "<leader>ctt", jester.run, "Run Jest test under cursor")
@@ -168,37 +185,69 @@ set_keymap("n", "<leader>ctf", jester.run_file, "Run Jest tests in current file"
 set_keymap("n", "<leader>ctl", jester.run_last, "Re-run last Jest test")
 
 -- Copilot
---vim.keymap.set("i", "`", 'copilot#Accept("\\<CR>")', {
---expr = true,
---replace_keycodes = false,
---})
---vim.g.copilot_no_tab_map = true
+-- TODO: (insane): Fork CopilotChat.nvim, and modify this file to allow you to call it smokes: https://github.com/CopilotC-Nvim/CopilotChat.nvim/blob/feca60cf0ae08d866ba35cc8a95d12941ccc4f59/lua/CopilotChat/prompts.lua#L5
+-- TODO: "fix" and "fix diagnostics" - https://github.com/CopilotC-Nvim/CopilotChat.nvim
+
 local chat = require("CopilotChat")
 local CopilotChatSelect = require("CopilotChat.select")
 
-local function ask_copilot(question, selectionMethod, callback)
-    return function()
-        chat.ask(question, {
-            selection = CopilotChatSelect[selectionMethod],
-            callback = callback or function() end,
-        })
-    end
+local function ask_copilot(prompt, selectionMethod, callback)
+	return function()
+		local question
+		if prompt ~= nil and prompt ~= "" then
+			question = prompt
+		else
+			question = vim.fn.input("Quick Chat: ")
+		end
+		-- TODO: Bail if no question
+		chat.ask(question, {
+			selection = CopilotChatSelect[selectionMethod],
+			callback = callback or function() end,
+		})
+	end
 end
 
 -- Function which uses set_keymap and ask_copilot to create the same keymap for visual and normal mode
-local function set_copilot_keymap(key, question, desc, callback)
-    set_keymap("n", key, ask_copilot(question, "buffer", callback), desc .. " current buffer")
-    set_keymap("v", key, ask_copilot(question, "visual", callback), desc .. " selection")
+local function set_copilot_ask_keymap(key, question, desc, callback)
+	set_keymap("n", key, ask_copilot(question, "buffer", callback), desc .. " current buffer")
+	set_keymap("v", key, ask_copilot(question, "visual", callback), desc .. " selection")
 end
 
--- TODO: "fix" and "fix diagnostics" - https://github.com/CopilotC-Nvim/CopilotChat.nvim
-set_keymap("n", "<leader>cc", "<cmd>CopilotChatOpen<CR>", "Open Copilot chat window")
+local function open_copilot_chat(selectionMethod)
+	return function()
+		chat.open({
+			selection = CopilotChatSelect[selectionMethod],
+		})
+	end
+end
+
+-- Open Copilot chat window fully
+set_keymap("n", "<leader>C", open_copilot_chat("buffer"), "Open Copilot chat window")
+set_keymap("v", "<leader>C", open_copilot_chat("visual"), "Open Copilot chat window")
+
+-- Ask Copilot a single question
+set_copilot_ask_keymap("<leader>cc", nil, "Open Copilot chat window")
 
 set_keymap("n", "<leader>gd", "<cmd>CopilotChatCommit<CR>", "Generate a commit message for the current diff")
 set_keymap("n", "<leader>gs", "<cmd>CopilotChatCommitStaged<CR>", "Generate a commit message for the staged changes")
 
+-- Open options in telescope
+
+local CopilotChatTelescope = require("CopilotChat.integrations.telescope")
+-- Open help actions with telescope
+set_keymap("n", "<leader>ch", function()
+	local actions = require("CopilotChat.actions").help_actions()
+	CopilotChatTelescope.pick(actions)
+end, "CopilotChat - Help actions")
+
+-- Show prompts actions with telescope
+set_keymap("n", "<leader>cp", function()
+	local actions = require("CopilotChat.actions").prompt_actions()
+	CopilotChatTelescope.pick(actions)
+end, "CopilotChat - Prompt actions")
+
 -- Explain
-set_copilot_keymap("<leader>ce", "Please explain how this code works", "Ask Copilot to explain")
+set_copilot_ask_keymap("<leader>ce", "Please explain how this code works", "Ask Copilot to explain")
 
 -- TODO: This seems inconsistent
 -- Review
@@ -206,14 +255,27 @@ set_keymap("n", "<leader>cr", "<cmd>CopilotChatReview<CR>", "Ask copilot to revi
 set_keymap("v", "<leader>cr", "<cmd>CopilotChatReview<CR>", "Ask copilot to review selection")
 
 -- Optimize
-set_copilot_keymap(
-    "<leader>co",
-    "Please optimize this code to improve performance and readablilty",
-    "Ask Copilot to optimize"
+set_copilot_ask_keymap(
+	"<leader>co",
+	"Please optimize this code to improve performance and readablilty",
+	"Ask Copilot to optimize"
 )
 
 -- Document
-set_copilot_keymap("<leader>cd", "Please add documentation comment(s) for this code", "Ask Copilot to document")
+set_copilot_ask_keymap("<leader>cd", "Please add documentation comment(s) for this code", "Ask Copilot to document")
 
 -- Generate Tests
-set_copilot_keymap("<leader>ct", "Please generate tests for this code", "Ask Copilot to document")
+set_copilot_ask_keymap("<leader>ct", "Please generate tests for this code", "Ask Copilot to document")
+
+-- Quick win
+set_copilot_ask_keymap("<leader>cq", "Give me a quick win", "Ask Copilot for a quick win")
+
+--set_keymap("n", "<leader>9", function()
+--local buf = vim.api.nvim_create_buf(false, true)
+--vim.api.nvim_buf_set_lines(buf, 0, -1, true, {"test", "text"})
+--local opts = {relative='cursor', width=10, height=2, col=0, row=1, anchor='NW', style='minimal'}
+--local win = vim.api.nvim_open_win(buf, false, opts)
+-- optional: change highlight, otherwise Pmenu is used
+-- vim.api.nvim_win_set_option(win, 'winhl', 'Normal:MyHighlight')
+--end
+--)
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,17 +1,17 @@
 local language_servers = {
-    "stylua",
-    "lua-language-server",
-    "shellcheck",
-    "shfmt",
-    "flake8",
-    "typescript-language-server",
-    "tailwindcss-language-server",
-    "svelte-language-server",
-    "eslint-lsp",
-    "prettier",
-    "docker-compose-language-service",
-    "dockerfile-language-server",
-    "gdtoolkit",
+	"stylua",
+	"lua-language-server",
+	"shellcheck",
+	"shfmt",
+	"flake8",
+	"typescript-language-server",
+	"tailwindcss-language-server",
+	"svelte-language-server",
+	"eslint-lsp",
+	"prettier",
+	"docker-compose-language-service",
+	"dockerfile-language-server",
+	"gdtoolkit",
 }
 
 -- TODO Leftover imports from last config, investigate
@@ -19,231 +19,262 @@ local language_servers = {
 -- 'jose-elias-alvarez/nvim-lsp-ts-utils',
 
 return {
-    -- Github Copilot
-    {
-        "zbirenbaum/copilot.lua",
-        cmd = "Copilot",
-        event = "InsertEnter",
-        config = function()
-            require("copilot").setup({
-                suggestion = { enabled = false },
-                panel = { enabled = false },
-            })
-        end,
-    },
-    {
-        "zbirenbaum/copilot-cmp",
-        dependencies = {
-            "zbirenbaum/copilot.lua",
-        },
-        config = function()
-            require("copilot_cmp").setup()
-        end,
-    },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        branch = "canary",
-        dependencies = {
-            { "zbirenbaum/copilot.lua" },
-            { "nvim-lua/plenary.nvim" },
-        },
-        opts = {
-            debug = true,     -- Enable debugging
-            window = {
-                layout = "float", -- Set layout to float
-                title = "GitHub Copilot", -- Set title
-            },
-            submit_prompt = {
-                normal = "<CR>",
-                insert = "<CR><CR>",
-            },
-        },
-    },
-    -- autocompletion
-    {
-        "hrsh7th/nvim-cmp",
-        version = false, -- last release is way too old
-        event = "InsertEnter",
-        dependencies = {
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-buffer",
-            "hrsh7th/cmp-path",
-            "hrsh7th/cmp-vsnip",
-            "hrsh7th/vim-vsnip",
-            "hrsh7th/cmp-nvim-lsp-signature-help",
-            "zbirenbaum/copilot-cmp",
-        },
-        opts = function(_, opts)
-            local cmp = require("cmp")
-            return {
-                completion = {
-                    completeopt = "menu,menuone,noinsert",
-                },
-                snippet = {
-                    expand = function(args)
-                        vim.fn["vsnip#anonymous"](args.body)
-                    end,
-                },
-                mapping = {
-                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-                    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
-                    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
-                    ["<C-p>"] = cmp.mapping(cmp.mapping.scroll_docs(-3)),
-                    ["<C-n>"] = cmp.mapping(cmp.mapping.scroll_docs(3)),
-                },
-                sources = {
-                    { name = "copilot" },
-                    { name = "nvim_lsp_signature_help" },
-                    { name = "nvim_lsp" },
-                    { name = "vsnip" },
-                    { name = "buffer" },
-                    { name = "path" },
-                },
-            }
-        end,
-    },
-    -- lsp
-    {
-        "neovim/nvim-lspconfig",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = {
-            {
-                "folke/neoconf.nvim",
-                cmd = "Neoconf",
-                config = true,
-            },
-            { "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
-            "mason.nvim",
-            "williamboman/mason-lspconfig.nvim",
-            "hrsh7th/cmp-nvim-lsp",
-            "jose-elias-alvarez/typescript.nvim", -- TypeScript
-            "ray-x/lsp_signature.nvim",
-            {
-                "j-hui/fidget.nvim",
-                opts = { tag = "legacy" }, -- Show LSP progress
-            },
-        },
-        opts = {
-            servers = {
-                tailwindcss = {},
-                lua_ls = {
-                    settings = {
-                        Lua = {
-                            workspace = {
-                                checkThirdParty = false,
-                            },
-                        },
-                    },
-                },
-                gdscript = {},
-            },
-            setup = {
-                tsserver = function(_, opts)
-                    require("typescript").setup({ server = opts })
-                    return true
-                end,
-            },
-        },
-        config = function(_, opts)
-            -- This is mostly lifted from LazyVim
-            local mlsp = require("mason-lspconfig")
-            local available = mlsp.get_available_servers()
+	-- Github Copilot
+	{
+		"zbirenbaum/copilot.lua",
+		cmd = "Copilot",
+		event = "InsertEnter",
+		config = function()
+			require("copilot").setup({
+				suggestion = { enabled = false },
+				panel = { enabled = false },
+			})
+		end,
+	},
+	{
+		"zbirenbaum/copilot-cmp",
+		dependencies = {
+			"zbirenbaum/copilot.lua",
+		},
+		config = function()
+			require("copilot_cmp").setup()
+		end,
+	},
+	{
+		"CopilotC-Nvim/CopilotChat.nvim",
+		branch = "canary",
+		dependencies = {
+			{ "zbirenbaum/copilot.lua" },
+			{ "nvim-lua/plenary.nvim" },
+		},
+		opts = {
+			debug = true, -- Enable debugging
+			window = {
+				layout = "float", -- Set layout to float
+				title = "GitHub Copilot", -- Set title
+				width = 0.8,
+				height = 0.8,
+			},
+			submit_prompt = {
+				normal = "<CR>",
+				insert = "<CR><CR>",
+			},
+		},
+	},
+	-- autocompletion
+	{
+		"hrsh7th/nvim-cmp",
+		version = false, -- last release is way too old
+		event = "InsertEnter",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
 
-            local ensure_installed = {} ---@type string[]
-            local servers = opts.servers
-            local capabilities =
-                require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-            local function setup(server)
-                local server_opts = vim.tbl_deep_extend("force", {
-                    capabilities = vim.deepcopy(capabilities),
-                }, servers[server] or {})
+			-- New and experimental begin
+			"hrsh7th/cmp-cmdline",
+			-- New and experimental end
+			"hrsh7th/cmp-vsnip",
+			"hrsh7th/vim-vsnip",
+			"hrsh7th/cmp-nvim-lsp-signature-help",
+			"zbirenbaum/copilot-cmp",
+			"onsails/lspkind.nvim",
+		},
+		opts = function(_, opts)
+			local cmp = require("cmp")
+			local lspkind = require("lspkind")
+			return {
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				formatting = {
+					format = lspkind.cmp_format({
+						mode = "symbol_text",
+						max_width = 100,
+						symbol_map = { Copilot = "" },
+					}),
+				},
+				snippet = {
+					expand = function(args)
+						vim.fn["vsnip#anonymous"](args.body)
+					end,
+				},
+				mapping = {
+					["<CR>"] = cmp.mapping.confirm({ select = true }),
+					["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+					["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
+					["<C-p>"] = cmp.mapping(cmp.mapping.scroll_docs(-3)),
+					["<C-n>"] = cmp.mapping(cmp.mapping.scroll_docs(3)),
+				},
+				sources = {
+					{ name = "copilot" },
+					{ name = "nvim_lsp_signature_help" },
+					{ name = "nvim_lsp" },
+					{ name = "vsnip" },
+					{ name = "buffer" },
+					{ name = "path" },
+				},
+				sorting = {
+					priority_weight = 2,
+					comparators = {
+						cmp.config.compare.exact, -- Prioritize exact matches over copilot suggestions
+						-- require("copilot_cmp.comparators").prioritize,
+						cmp.config.compare.offset,
+						cmp.config.compare.score,
+						cmp.config.compare.recently_used,
+						cmp.config.compare.locality,
+						cmp.config.compare.kind,
+						cmp.config.compare.sort_text,
+						cmp.config.compare.length,
+						cmp.config.compare.order,
+					},
+				},
+			}
+		end,
+	},
+	-- lsp
+	{
+		"neovim/nvim-lspconfig",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			{
+				"folke/neoconf.nvim",
+				cmd = "Neoconf",
+				config = true,
+			},
+			{ "folke/neodev.nvim", opts = { experimental = { pathStrict = true } } },
+			"mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"hrsh7th/cmp-nvim-lsp",
+			"jose-elias-alvarez/typescript.nvim", -- TypeScript
+			"ray-x/lsp_signature.nvim",
+			{
+				"j-hui/fidget.nvim",
+				opts = { tag = "legacy" }, -- Show LSP progress
+			},
+		},
+		opts = {
+			servers = {
+				tailwindcss = {},
+				lua_ls = {
+					settings = {
+						Lua = {
+							workspace = {
+								checkThirdParty = false,
+							},
+						},
+					},
+				},
+				gdscript = {},
+			},
+			setup = {
+				tsserver = function(_, opts)
+					require("typescript").setup({ server = opts })
+					return true
+				end,
+			},
+		},
+		config = function(_, opts)
+			-- This is mostly lifted from LazyVim
+			local mlsp = require("mason-lspconfig")
+			local available = mlsp.get_available_servers()
 
-                if opts.setup[server] then
-                    if opts.setup[server](server, server_opts) then
-                        return
-                    end
-                elseif opts.setup["*"] then
-                    if opts.setup["*"](server, server_opts) then
-                        return
-                    end
-                end
-                require("lspconfig")[server].setup(server_opts)
-            end
-            for server, server_opts in pairs(servers) do
-                if server_opts then
-                    server_opts = server_opts == true and {} or server_opts
-                    -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
-                    if server_opts.mason == false or not vim.tbl_contains(available, server) then
-                        setup(server)
-                    else
-                        ensure_installed[#ensure_installed + 1] = server
-                    end
-                end
-            end
+			local ensure_installed = {} ---@type string[]
+			local servers = opts.servers
+			local capabilities =
+				require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+			local function setup(server)
+				local server_opts = vim.tbl_deep_extend("force", {
+					capabilities = vim.deepcopy(capabilities),
+				}, servers[server] or {})
 
-            require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
-            require("mason-lspconfig").setup_handlers({ setup })
-            --require("lsp_signature").setup({}) -- TODO: Not super thrilled with the defaults, look into later
-            -- Consider "Issafalcon/lsp-overloads.nvim",
-            -- Format on save
-            vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
-        end,
-    },
-    -- formatters
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        event = { "BufReadPre", "BufNewFile" },
-        dependencies = { "mason.nvim" },
-        opts = function()
-            local nls = require("null-ls")
-            return {
-                sources = {
-                    -- nls.builtins.formatting.prettierd,
-                    nls.builtins.formatting.stylua,
-                    nls.builtins.diagnostics.flake8,
-                },
-            }
-        end,
-    },
-    --- DAP
-    {
-        "mfussenegger/nvim-dap",
-        event = { "BufReadPre", "BufNewFile" },
-        config = function()
-            local dap = require("dap")
-            dap.adapters.godot = {
-                type = "server",
-                host = "127.0.0.1",
-                port = 6006,
-            }
-            dap.configurations.gdscript = {
-                {
-                    type = "godot",
-                    request = "launch",
-                    name = "Launch scene",
-                    project = "${workspaceFolder}",
-                    launch_scene = true,
-                },
-            }
-        end,
-    },
-    --- lsp-management
-    {
-        "williamboman/mason.nvim",
-        cmd = "Mason",
-        opts = {
-            ensure_installed = language_servers,
-        },
-        ---@param opts MasonSettings | {ensure_installed: string[]}
-        config = function(plugin, opts)
-            require("mason").setup()
-            local mr = require("mason-registry")
-            for _, tool in ipairs(opts.ensure_installed) do
-                local p = mr.get_package(tool)
-                if not p:is_installed() then
-                    p:install()
-                end
-            end
-        end,
-    },
+				if opts.setup[server] then
+					if opts.setup[server](server, server_opts) then
+						return
+					end
+				elseif opts.setup["*"] then
+					if opts.setup["*"](server, server_opts) then
+						return
+					end
+				end
+				require("lspconfig")[server].setup(server_opts)
+			end
+			for server, server_opts in pairs(servers) do
+				if server_opts then
+					server_opts = server_opts == true and {} or server_opts
+					-- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+					if server_opts.mason == false or not vim.tbl_contains(available, server) then
+						setup(server)
+					else
+						ensure_installed[#ensure_installed + 1] = server
+					end
+				end
+			end
+
+			require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
+			require("mason-lspconfig").setup_handlers({ setup })
+			--require("lsp_signature").setup({}) -- TODO: Not super thrilled with the defaults, look into later
+			-- Consider "Issafalcon/lsp-overloads.nvim",
+			-- Format on save
+			vim.cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
+		end,
+	},
+	-- formatters
+	{
+		"jose-elias-alvarez/null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = { "mason.nvim" },
+		opts = function()
+			local nls = require("null-ls")
+			return {
+				sources = {
+					-- nls.builtins.formatting.prettierd,
+					nls.builtins.formatting.stylua,
+					nls.builtins.diagnostics.flake8,
+				},
+			}
+		end,
+	},
+	--- DAP
+	{
+		"mfussenegger/nvim-dap",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			local dap = require("dap")
+			dap.adapters.godot = {
+				type = "server",
+				host = "127.0.0.1",
+				port = 6006,
+			}
+			dap.configurations.gdscript = {
+				{
+					type = "godot",
+					request = "launch",
+					name = "Launch scene",
+					project = "${workspaceFolder}",
+					launch_scene = true,
+				},
+			}
+		end,
+	},
+	--- lsp-management
+	{
+		"williamboman/mason.nvim",
+		cmd = "Mason",
+		opts = {
+			ensure_installed = language_servers,
+		},
+		---@param opts MasonSettings | {ensure_installed: string[]}
+		config = function(plugin, opts)
+			require("mason").setup()
+			local mr = require("mason-registry")
+			for _, tool in ipairs(opts.ensure_installed) do
+				local p = mr.get_package(tool)
+				if not p:is_installed() then
+					p:install()
+				end
+			end
+		end,
+	},
 }
