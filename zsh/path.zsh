619a7f821df2f82d3dad898324ffd490614b33e3
Willow Hayward <willow@whc.fyi>
2024-06-02 21:40:06 +1000

    feat(zsh): Added cargo to path

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -1,4 +1,7 @@
 # added by pipx (https://github.com/pipxproject/pipx)
 export PATH="/home/whayward/.local/bin:$PATH"
 # Android studio
-export PATH="${PATH};~/android-studio/bin"
+export PATH="${PATH}:~/android-studio/bin"
+
+export PATH="${PATH}:$HOME/.cargo/bin"
+#$HOME/.cargo/env
