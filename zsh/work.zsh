c86fe8182c42407e1036ac37a3cf753372a30579
Willow Hayward <willow@whc.fyi>
2024-06-04 20:43:34 +1000

    feat(zsh): added some generic work-specific bindings

--- zsh/work.zsh
+++ zsh/work.zsh
@@ -1,2 +1,6 @@
 # This file is for configuration particular to my workplace, and will not always be committed
 # It will be loaded nearly last, and only if WHC_WORK is set
+
+# TaskWarrior - Assumes context "work" set to "project:work" exists
+task context work 
+alias taw='task add project:work'
