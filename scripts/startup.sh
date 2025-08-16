9a716e6283ce9754dfe76c34afe0f30079a4eb56
Willow Hayward <willow@whc.fyi>
2025-08-16 18:44:23 +1000

    Added startup script for crontab

---
+++ scripts/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Commands to run on computer startup
+# Set up contab of "@reboot  /home/willowhayward/dotfiles/scripts/startup.sh"
+export MOZ_USE_XINPUT2=1
+setxkbmap -option "caps:swapescape"
+onboard
